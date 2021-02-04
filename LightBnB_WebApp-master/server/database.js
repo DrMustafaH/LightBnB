b9fa17efc1003e5ef9a548eff001b8ac59ca7e58
DrMustafaH <mustafaharith@gmail.com>
2021-02-04 15:12:07 -0800

    Implement search for properties by editing getAllProperties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -95,12 +95,54 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
+  const queryParams = [];
+  let queryStr = `SELECT
+properties.*,
+AVG(property_reviews.rating) as average_rating
+FROM
+properties
+JOIN property_reviews ON property_id = properties.id `;
 
-  return pool.query(`
-        SELECT * FROM properties
-        LIMIT $1
-        `, [limit])
-    .then(res => res.rows);
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryStr += `WHERE city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    if (queryParams.length === 1) {
+      queryStr += `WHERE owner_id = ${queryParams.length} `;
+    } else {
+      queryStr += `AND owner_id = ${queryParams.length} `;
+    }
+  }
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`, `${options.maximum_price_per_night}`);
+    if (queryParams.length === 2) {
+      queryStr += `WHERE cost_per_night > $${queryParams.length - 1} AND cost_per_night < $${queryParams.length} `;
+    } else {
+      queryStr += `AND cost_per_night > $${queryParams.length - 1} AND cost_per_night < $${queryParams.length} `;
+    }
+  }
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    if (queryParams.length === 1) {
+      queryStr += `WHERE rating >= $${queryParams.length} `;
+    } else {
+      queryStr += `AND rating >= $${queryParams.length} `;
+    }
+  }
+
+
+  queryParams.push(limit);
+  queryStr += `GROUP BY properties.id
+ORDER BY cost_per_night
+LIMIT $${queryParams.length};`
+
+  console.log("HERE", queryStr, queryParams);
+
+  return pool.query(queryStr, queryParams)
+    .then(res => res.rows)
+
 }
 
 exports.getAllProperties = getAllProperties;
